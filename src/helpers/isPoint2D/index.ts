f13677744e4adc8990af5b4531f0d624117031fc
hota1024 <hotalog@hota1024.com>
2020-03-08 23:24:03 +0900

    🏷️ Update isPoint2D helper

--- src/helpers/isPoint2D/index.ts
+++ src/helpers/isPoint2D/index.ts
@@ -1,9 +1,11 @@
+import { Point2D } from '../../types/Point2D'
+
 /**
  * Returns whether value is Point2D.
  *
  * @param value
  */
-export function isPoint2D(value: any) {
+export function isPoint2D(value: any): value is Point2D {
   return (
     typeof value === 'object' &&
     typeof value.x === 'number' &&
