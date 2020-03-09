d01c0d8c919fc3668d48db0c327b98a72ee8cab1
hota1024 <hotalog@hota1024.com>
2020-03-09 15:52:11 +0900

    🏷️ Add Component class

---
+++ src/abstracts/Component/index.ts
@@ -0,0 +1,18 @@
+import { ComponentInterface } from '../../interfaces/Component'
+
+/*
+ * Component class.
+ */
+export class Component implements ComponentInterface {
+  /**
+   * Call on frame start.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onFrameStart() {}
+
+  /**
+   * Call on frame end.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onFrameEnd() {}
+}
