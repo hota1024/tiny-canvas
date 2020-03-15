65d4fa0550520dbe0a22babc0258699d7e54d275
hota1024 <hotalog@hota1024.com>
2020-03-16 06:07:42 +0900

    🏷️ Add GridInterface

---
+++ src/interfaces/Grid/index.ts
@@ -0,0 +1,50 @@
+/*
+ * Grid interface.
+ */
+export interface GridInterface<T> {
+  /**
+   * Width.
+   */
+  readonly width: number
+
+  /**
+   * Height.
+   */
+  readonly height: number
+
+  /**
+   * Number of items.
+   */
+  readonly count: number
+
+  /**
+   * Get point data.
+   *
+   * @param x
+   * @param y
+   */
+  get(x: number, y: number): T
+
+  /**
+   * Set point data.
+   *
+   * @param x
+   * @param y
+   * @param data
+   */
+  set(x: number, y: number, data: T): void
+
+  /**
+   * Fill data.
+   *
+   * @param data
+   */
+  fill(data: T): void
+
+  /**
+   * Each data.
+   *
+   * @param callback
+   */
+  each(callback: (x: number, y: number, data: T) => void): void
+}
