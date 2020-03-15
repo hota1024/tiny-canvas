8c0f668fa4231b5816aa803ff2cd495af0e57622
hota1024 <hotalog@hota1024.com>
2020-03-16 07:19:03 +0900

    🏷️ Update Grid impl

--- src/impls/grids/Grid/index.ts
+++ src/impls/grids/Grid/index.ts
@@ -27,6 +27,19 @@ export class Grid<T> implements GridInterface<T> {
   private readonly data: T[]
 
   /**
+   * Grid constructor.
+   *
+   * @param width
+   * @param height
+   */
+  constructor(width: number, height: number) {
+    this.width = width
+    this.height = height
+
+    this.map(() => undefined)
+  }
+
+  /**
    * Get point data.
    *
    * @param x
@@ -67,6 +80,19 @@ export class Grid<T> implements GridInterface<T> {
     for (let y = 0; y < this.height; ++y) {
       for (let x = 0; x < this.width; ++x) {
         callback(x, y, this.get(x, y))
+      }
+    }
+  }
+
+  /**
+   * Map data.
+   *
+   * @param callback
+   */
+  map(callback: (x: number, y: number, data: T) => T) {
+    for (let y = 0; y < this.height; ++y) {
+      for (let x = 0; x < this.width; ++x) {
+        this.set(x, y, callback(x, y, this.get(x, y)))
       }
     }
   }
